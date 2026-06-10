b7d9f6a2a31bb1889d3cbe7ee21e0c62c87360cf
tub1k <gameryoutubik@gmail.com>
2026-06-10 17:14:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const admin = require('firebase-admin');
+const cors = require('cors');
 const { cert } = require('firebase-admin/app');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
